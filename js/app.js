0d776f424580021d5c4538daa5f95c9f8cdb4e32
Jacob Feiler <jacob.feiler@basestate.com.au>
2024-01-31 14:03:53 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,1 @@
+(()=>{var e=0;!function t(){var s,l=document.getElementsByClassName("slides");for(s=0;s<l.length;s++)l[s].style.opacity="0";++e>l.length&&(e=1),l[e-1].style.opacity="1",setTimeout(t,5e3)}()})();
